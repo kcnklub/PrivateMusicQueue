ae9c8449369e8cbe1f56c778b8099c774aeda64c
Oscar Lewczuk <odlmb7@mst.edu>
2019-05-02 16:26:48 -0500

    Added Scores

--- app/src/main/java/com/jukebox/hero/ui/adapters/TrackAdapter.kt
+++ app/src/main/java/com/jukebox/hero/ui/adapters/TrackAdapter.kt
@@ -78,7 +78,7 @@ class TrackAdapter(private var data: List<Track>, val context: Context, private 
                         }
                         FirebaseFirestore.getInstance().runTransaction{ p1 ->
                             Log.d(TAG, count.toString())
-                            val song = Song(songName, artistName, albumArtURL, songURI, count + 1)
+                            val song = Song(songName, artistName, albumArtURL, songURI, count + 1, 0)
                             p1.set(songRef, song)
                             null
                         }.addOnSuccessListener {
